f8b5c2f0e306cb346095fd08ade129e4fa24ceb5
yhc <qq2725761897@163.com>
2025-07-28 16:43:59 +0800

    finish lab4

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -2,9 +2,9 @@ package flik;
 
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
-        int i = 0;
+        Integer i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
-            if (!Flik.isSameNumber(i, j)) {
+            if (!Flik.isSameNumber(i, i)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
